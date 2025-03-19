a09d1b02d4313fbcb5ec6d637363a49804a2af5c
Lukáš Bystroň <lbystron@gmail.com>
2025-03-19 13:32:48 +0100

    Add files via upload

--- projekt_2.py
+++ projekt_2.py
@@ -32,51 +32,52 @@ def statistics(hadane_cislo, pocet_hadani, cas):
         "Čas": cas
     }
 
-def user_choice(uzivatel_cislo, pocet):
-    pass
+def user_choice(aa, cc, dd, gg):
+    pokracovani = True
+    while pokracovani:
+        try:
+            aa = int(input(">>> "))
+            if aa != dd:
+                cc += 1
+                print(f"{gg}\n{znak}")
+            else:
+                print(f"Correct, you've guessed the right number\nin {cc} guesses!\n{znak}\nThat´s amazing!")
+                pokracovani = False
+        except ValueError:
+            print("Numbers only")
 
-def evaluation(rozdil):
-    pass
+def bull(uz):
+        uz.count(uz)
 
 start = time() # stopky - zacatek
 znak = "-" * 47  # oddělovací čárka
 cisla_uzivatel = [] # seznam čísel od uživatele
 cisla_random = [] # seznam čísel od PC nahodne
 guesses = 0 #pocet pokusu
+pocet_cisel = []
 
 #uvitání hráče a představení hry
 print(f'''Hi there!\n{znak} 
 I've generated a random 4 digit number for you.
-Let's play a bulls and cows game.\n{znak}'''
+Let's play a bulls and cows game.\n{znak}\nEnter a number:\n{znak}'''
 )
 
 #vylosované číslo pro hádnání
 PC_nahodne = random_number()
 print("TEST tipované číslo:", PC_nahodne)
-
+uzivatel = int(input(">>> "))
+print(pocet_cisel,"\n", znak)
 
 #rozdělení čísla do seznamu od uživatele a nahodné
-
 rozdil_seznamu = [value for value in cisla_uzivatel if value in cisla_random] # vyhledani stejnych cisel uzivate vs PC
-
-pokracovani = True
-while pokracovani:
-    try:
-        uzivatel = int(input("Enter a number: "))
-        set_of_numbers(uzivatel, PC_nahodne)
-        if uzivatel != PC_nahodne:
-            guesses += 1
-            print(f">>> {uzivatel}\n{rozdil_seznamu}\n{znak}")
-        else:
-            print(f"Correct, you've guessed the right number\nin {guesses} guesses!\n{znak}\nThat´s amazing!")
-            pokracovani = False
-    except ValueError:
-        print("Numbers only")
-
-#set_of_numbers(uzivatel, PC_nahodne)
+bull(uz=cisla_uzivatel)
+set_of_numbers(cislo_od_uzivatele=uzivatel, cisla_od_PC=PC_nahodne)
+user_choice(aa=uzivatel, cc=guesses, dd=PC_nahodne, gg=pocet_cisel)
 end = time() # stopky - konec
 x = round(end - start,2) #výsledek jak dlouho hádal uživatel
 
+print("TEST uzivatel", uzivatel)
+print("TEST bull", bull)
 print("TEST vypisu", cisla_uzivatel)
 print("TEST vypisu", cisla_random)
 print("TEST vypis rozdilu", rozdil_seznamu)
